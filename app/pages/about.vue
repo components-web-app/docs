f56cbb8a648b9ab0820a2b3720a8cdd90745bb5f
silverbackdan <daniel@silverback.is>
2024-10-09 18:35:58 +0100

    Further content structure

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -10,94 +10,100 @@ useSeoMeta({
 </script>
 
 <template>
-  <UContainer>
-    <UPageHero
-      v-bind="page.hero"
-    >
-      <template #description>
-        <MDC :value="page.hero.description" />
-      </template>
-      <div class="flex justify-center">
-        <div class="w-full max-w-[840px] rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700 overflow-hidden">
-          <div style="padding:56.25% 0 0 0;position:relative;">
-            <iframe
-              src="https://player.vimeo.com/video/1010679128?badge=0&amp;autopause=0&amp;player_id=0"
-              frameborder="0"
-              allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
-              style="position:absolute;top:0;left:0;width:100%;height:100%;"
-              title="CWA Overview"
-            />
-          </div>
-        </div>
-      </div>
-    </UPageHero>
-    <UPage>
-      <UPageBody />
-
-      <UPageGrid>
-        <ULandingCard
-          v-for="(item, index) of page.features.items"
-          :key="index"
-          v-bind="item"
-        />
-      </UPageGrid>
-
-      <ULandingSection
-        v-for="(section, index) of page.sections"
-        :key="index"
-        v-bind="section"
-      >
-        <template #title>
-          <MDC
-            v-if="section.title"
-            :value="section.title"
-          />
-        </template>
-
-        <template #description>
-          <MDC
-            v-if="section.description"
-            :value="section.description"
-          />
-        </template>
-
-        <template #code>
-          <MDC
-            :value="section.code"
-            tag="pre"
-            class="prose prose-primary dark:prose-invert max-w-none"
-          />
-        </template>
-
-        <template #features>
-          <UPageGrid>
-            <UPageCard
-              v-for="(feature, index) in section.featureCards"
-              :key="index"
-              v-bind="feature"
-            />
-          </UPageGrid>
-        </template>
-      </ULandingSection>
-
-      <ULandingSection
-        :title="page.faq.title"
-        :description="page.faq.description"
-      >
-        <ULandingFAQ
-          :items="page.faq.items"
-          multiple
+  <div>
+    <div class="bg-primary/5">
+      <UContainer>
+        <UPageHero
+          v-bind="page.hero"
         >
-          <template #item="{ item }">
+          <template #description>
+            <MDC :value="page.hero.description" />
+          </template>
+          <div class="flex justify-center">
+            <div class="w-full max-w-[840px] rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700 overflow-hidden">
+              <div style="padding:56.25% 0 0 0;position:relative;">
+                <iframe
+                  src="https://player.vimeo.com/video/1010679128?badge=0&amp;autopause=0&amp;player_id=0"
+                  frameborder="0"
+                  allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+                  style="position:absolute;top:0;left:0;width:100%;height:100%;"
+                  title="CWA Overview"
+                />
+              </div>
+            </div>
+          </div>
+        </UPageHero>
+      </UContainer>
+    </div>
+    <UContainer>
+      <UPage>
+        <UPageBody />
+
+        <UPageGrid>
+          <UPageCard
+            v-for="(item, index) of page.pageCards.items"
+            :key="index"
+            v-bind="item"
+          />
+        </UPageGrid>
+
+        <ULandingSection
+          v-for="(section, index) of page.sections"
+          :key="index"
+          v-bind="section"
+        >
+          <template #title>
             <MDC
-              :value="item.content"
-              class="prose prose-primary dark:prose-invert max-w-none text-gray-500 dark:text-gray-400"
+              v-if="section.title"
+              :value="section.title"
             />
           </template>
-        </ULandingFAQ>
-      </ULandingSection>
-    </UPage>
-  </UContainer>
+
+          <template #description>
+            <MDC
+              v-if="section.description"
+              :value="section.description"
+            />
+          </template>
+
+          <template #code>
+            <MDC
+              :value="section.code"
+              tag="pre"
+              class="prose prose-primary dark:prose-invert max-w-none"
+            />
+          </template>
+
+          <template #features>
+            <UPageGrid>
+              <UPageCard
+                v-for="(feature, index) in section.featureCards"
+                :key="index"
+                v-bind="feature"
+              />
+            </UPageGrid>
+          </template>
+        </ULandingSection>
+
+        <ULandingSection
+          :title="page.faq.title"
+          :description="page.faq.description"
+        >
+          <ULandingFAQ
+            :items="page.faq.items"
+            multiple
+          >
+            <template #item="{ item }">
+              <MDC
+                :value="item.content"
+                class="prose prose-primary dark:prose-invert max-w-none text-gray-500 dark:text-gray-400"
+              />
+            </template>
+          </ULandingFAQ>
+        </ULandingSection>
+      </UPage>
+    </UContainer>
+  </div>
 </template>
 
 <style lang="postcss">
--- app/pages/built-for-business.vue
+++ app/pages/built-for-business.vue
@@ -10,26 +10,29 @@ useSeoMeta({
 </script>
 
 <template>
-  <UContainer>
-    <UPageHero
-      v-bind="page.hero"
-    >
-      <template #description>
-        <MDC :value="page.hero.description" />
-      </template>
-      <div class="flex justify-center">
-        <NuxtImg
-          src="https://picsum.photos/id/180/640/360"
-          class="w-full max-w-[640px] rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
-          width="640"
-          height="360"
-        />
-      </div>
-    </UPageHero>
-    <UPage>
-      <UPageBody />
-    </UPage>
-  </UContainer>
+  <div>
+    <div class="bg-primary/5">
+      <UContainer>
+        <UPageHero
+          v-bind="page.hero"
+        >
+          <template #description>
+            <MDC :value="page.hero.description" />
+          </template>
+          <div class="flex justify-center">
+            <NuxtImg
+              src="https://picsum.photos/id/180/640/360"
+              class="w-full max-w-[640px] rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
+              width="640"
+              height="360"
+            />
+          </div>
+        </UPageHero>
+      </UContainer>
+    </div>
+    <UContainer>
+    </UContainer>
+  </div>
 </template>
 
 <style scoped lang="postcss">
