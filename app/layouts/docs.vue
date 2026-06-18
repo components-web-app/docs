098c95d5bee6895ab4ae5a62bd376adb56146303
silverbackdan <daniel@silverback.is>
2026-06-18 22:44:27 +0100

    Nav: collapse all sections by default, expand first three on desktop; move Built-ins into Components sub-menu

--- app/layouts/docs.vue
+++ app/layouts/docs.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
 import type { ContentNavigationItem } from '@nuxt/content'
+import { useBreakpoints, breakpointsTailwind } from '@vueuse/core'
 
 const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const isDesktop = breakpoints.greaterOrEqual('lg')
+
+const EXPANDED_ON_DESKTOP = ['/getting-started', '/guides', '/core-concepts']
+
+const mappedNavigation = computed<ContentNavigationItem[]>(() => {
+  return navigation?.value?.map(item => ({
+    ...item,
+    collapsed: isDesktop.value
+      ? !EXPANDED_ON_DESKTOP.some(p => item.path?.startsWith(p))
+      : true
+  })) ?? []
+})
 </script>
 
 <template>
@@ -12,7 +27,7 @@ const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
           <template #top>
             <UContentSearchButton size="md" :collapsed="false" />
           </template>
-          <UContentNavigation :navigation="navigation" />
+          <UContentNavigation :navigation="mappedNavigation" />
         </UPageAside>
       </template>
 
